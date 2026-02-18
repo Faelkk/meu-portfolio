0957c3cd6024512f634fbc6f559c5e86056976c6
Faelkk <Achten.rafael16@gmail.com>
2026-02-18 11:10:48 -0300

    feat: add components,header,modal, hooks, utils functions,assets

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,67 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useIsVisibleItemOnScreen } from "../../app/hooks/useIsVisibleItemOnScreen";
+import { useWindowWidth } from "../../app/hooks/useWindowWidth";
+import NavigationHeader from "../../views/Header/Components/NavigationHeader";
+import Modal from "../Modal/Modal";
+import useModal from "../Modal/useModal";
+import { cn } from "../../app/utils/cn/cn";
+import logoIcon from "../../assets/icons/android-chrome-512x512_1.svg";
+
+const Header = () => {
+  const { isVisible, elementRef } = useIsVisibleItemOnScreen();
+  const { isVisibleModal, handleOpenModal, handleCloseModal } = useModal();
+  const { width } = useWindowWidth();
+
+  return (
+    <header className="flex justify-center ">
+      <nav
+        ref={elementRef}
+        className={cn(
+          "flex items-center border-b border-gray-700  justify-between w-[95%] pt-6 pb-6  ",
+          isVisible ? " animate-startSlideDown" : ""
+        )}
+      >
+        <a
+          href="https://github.com/Faelkk"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3 ml-4"
+        >
+          <figure className="border-2 border-gray-950 flex items-center rounded-full p-[3px]">
+            <img
+              src={logoIcon}
+              className="w-6 h-6 2xl:w-8 2xl:h-8"
+              alt="Logo"
+            />
+          </figure>
+          <h1 className="font-bold text-xl 2xl:text-[1.3rem] text-gray-950">Rafael</h1>
+        </a>
+
+        {width >= 768 ? (
+          <NavigationHeader ClassNameUL="flex gap-6 mr-4 2xl:text-[1.1rem]" />
+        ) : (
+          <>
+            <button onClick={handleOpenModal} aria-label="Open Menu">
+              <HamburgerMenuIcon className="w-6 h-6" />
+            </button>
+
+            <Modal
+              classNameContent="fixed top-0 w-full h-[50%] rounded-[6px] bg-slate-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:animate-startSlideDown"
+              open={isVisibleModal}
+              onClose={handleCloseModal}
+            >
+              <NavigationHeader
+                ClassNameUL="flex flex-col gap-2"
+                ClassNameNav="flex h-full w-full justify-center items-center"
+                classNameLI="text-2xl font-bold text-gray-900"
+                onClickNavigate={handleCloseModal}
+              />
+            </Modal>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import type { ReactNode } from "react";
+import { cn } from "../../app/utils/cn/cn";
+
+
+
+
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  classNameContent?: string;
+  classNameOverlay?: string;
+  children: ReactNode;
+}
+
+const Modal = ({
+  open,
+  classNameContent,
+  classNameOverlay,
+  children,
+  onClose,
+}: ModalProps) => (
+  <Dialog.Root open={open} onOpenChange={onClose}>
+    <Dialog.Portal>
+      <Dialog.Overlay
+        className={cn("fixed inset-0  z-51   ", classNameOverlay)}
+      />
+      <Dialog.Content className={cn(classNameContent)}>
+        <button
+          className="absolute right-0 top-0 outline-none "
+          aria-label="Close"
+          onClick={onClose}
+        >
+          <Cross2Icon className="h-7 w-7" />
+        </button>
+
+        {children}
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
+
+export default Modal;
