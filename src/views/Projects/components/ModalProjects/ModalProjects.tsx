e5f5e5a849dca6d5035e19f6c651608046846d31
Faelkk <Achten.rafael16@gmail.com>
2026-02-18 16:48:32 -0300

    fix: fix ui color for dark mode in project modal

--- src/views/Projects/components/ModalProjects/ModalProjects.tsx
+++ src/views/Projects/components/ModalProjects/ModalProjects.tsx
@@ -29,7 +29,7 @@ const ModalProjects = ({
           classNameContent="bg-gray-50 dark:bg-woodsmoke-900 flex w-[80%] small:w-[50%]  md:w-[90%] h-[85%]  overflow-auto md:overflow-hidden  flex-col md:flex-row items-center md:max-w-[1100px]  md:h-auto fixed top-[50%] left-[50%]  translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-slate-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none  "
         >
           <section className="flex items-stretch  gap-4 flex-col md:flex-row   w-full">
-            <section className="flex p-5 rounded-md  flex-col items-center justify-center bg-woodsmoke-800 md:pb-3 md:px-0 ">
+            <section className="flex p-5 rounded-md  flex-col items-center justify-center bg-gray-300 dark:bg-woodsmoke-800 md:pb-3 md:px-0 ">
               <section className="w-full md:h-96 md:w-60 flex  justify-center ">
                 <img
                   src={projectModal.cardimage}
@@ -67,7 +67,7 @@ const ModalProjects = ({
                 </figure>
               </a>
 
-              <div className="bg-woodsmoke-800  my-3 md:my-0 flex flex-col gap-6  p-3 ">
+              <div className="bg-gray-300 dark:bg-woodsmoke-800  my-3 md:my-0 flex flex-col gap-6  p-3 ">
                 <p className="text-sm px-3 mt-3 md:h-35 flex h-full w-full   text-woodsmoke-900 dark:text-woodsmoke-200 ">
                   {projectModal?.description}
                 </p>
