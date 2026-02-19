09dfb5a63a08131e71f3424fff096a861cd3bcc0
Faelkk <Achten.rafael16@gmail.com>
2026-02-18 23:43:02 -0300

    refactor: refactor color style in btnTop

--- src/views/BtnTop/Button.tsx
+++ src/views/BtnTop/Button.tsx
@@ -10,7 +10,7 @@ const ButtonScrollTop = () => {
 
   return (
     <button className="fixed bottom-2 right-2 cursor-pointer" onClick={handleClick}>
-      <ArrowUpIcon className="w-6 h-6" />
+      <ArrowUpIcon className="w-6 h-6 text-woodsmoke-950 dark:text-gray-50" />
     </button>
   );
 };
